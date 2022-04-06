928b4ac9b8e1cfa09e616bb4b760e9061588d940
Aiden <sbjang01@gsenc.com>
2022-04-06 22:17:40 +0900

    index.js updated

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
-const app = express();
+const app = express()
+
+app.use(express.json())
 
 app.get('/',(req,res) => {
     res.json({
         status: 'ok'
     })
+})
+
+app.post('/post', (req,res) => {
+    user = req.body.user
+    res.json({user:user})
 })
 
 const port = 8080
